105814f45b8ea13cc1015d6ce4639dabc5b7b881
Patruni Dileep <pd9505424580@gmail.com>
2023-07-01 15:34:58 +0530

    adding expense.py

---
+++ expense.py
@@ -0,0 +1,8 @@
+def expenseCreation():
+    
+    date = input("please enter date: ")
+    category = input("please enter category: ")
+    discreption = input("please enter discreption: ")
+    amount = input("please enter amount: ")
+
+    return date, category, discreption, amount
