1f25dbda7c0571ed147e9578a893a383fb0b6f06
Sam12993a <gragerssam@gmail.com>
2023-07-02 15:16:42 +0530

    Amount filter updated

--- amountFilter.py
+++ amountFilter.py
@@ -1,11 +1,12 @@
 
 
-    
-file=open(".\expense\Demo.csv", "r")  # open file
-data= file.readline()                 # readline 
-while data != "":
-    data = file.readline()
-    line= data.split(",")
-    if line[0]=="tom":             # Actual Filter Happening
-        print(line)
- 
+def amountfilters(amount): 
+    amount = int(amount)
+    file=open(".\Expense_project\expenseDetails.csv", "r")  # open file
+    data= file.readline()                 # readline 
+    while data !="":
+        data = file.readline()
+        line = data.split(",")
+        if line[3]==amount:             # Actual Filter Happening
+            print(line)
+            
